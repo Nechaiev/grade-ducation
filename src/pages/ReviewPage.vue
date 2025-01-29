9ff8f28e88bf89503dd6c33e8764c9e52822aa73
Stanislav <stanislav.nechaev@gmail.com>
2025-01-29 03:05:21 +0100

    add ReviewPage.vue

---
+++ src/pages/ReviewPage.vue
@@ -0,0 +1,11 @@
+<script setup>
+import ReviewWidget from "../components/ReviewWidget.vue";
+const API_URL = "https://679955c6be2191d708b2a655.mockapi.io/reviews/1";
+</script>
+
+<template>
+  <div class="h-screen flex items-center justify-center bg-gray-50">
+    <ReviewWidget :apiUrl="API_URL" />
+  </div>
+</template>
+
