79190eb1b364814da82dc56834934df80c86b4a2
Stanislav <stanislav.nechaev@gmail.com>
2025-01-29 23:47:35 +0100

    add fonts

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        formular: ['Formular', 'sans-serif'],
+        formularMedium: ['Formular-Medium', 'sans-serif'],
+        greenwichMedium: ['Greenwich-Medium', 'sans-serif'],
+      },
+      colors: {
+        customBlue: "#1E3A8A",
+        customGreen: "#10B981",
+      },
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(to right, #ff7e5f, #feb47b)',
+      }
+    }
+  },
+  plugins: [],
+};
