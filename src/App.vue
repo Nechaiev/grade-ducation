a3f0f347f3167dcc425f6c3c06bcbabb9442c4c7
Stanislav <stanislav.nechaev@gmail.com>
2025-01-29 03:13:24 +0100

    adding switchLanguage button, router and navigation

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { useI18n } from "vue-i18n";
+
+const { t } = useI18n();
+const { locale } = useI18n();
+
+const switchLanguage = (lang) => {
+  locale.value = lang;
+};
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+    <nav class="p-4 bg-white shadow-md flex justify-between items-center">
+      <div class="space-x-4">
+        <router-link to="/" class="text-blue-500 hover:underline">{{ t("home") }}</router-link>
+        <router-link to="/reviews" class="text-blue-500 hover:underline">{{ t("feedback") }}</router-link>
+      </div>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+      <div>
+        <button @click="switchLanguage('uk')" class="px-3 py-1 border rounded-md mx-1">🇺🇦 UA</button>
+        <button @click="switchLanguage('en')" class="px-3 py-1 border rounded-md mx-1">🇬🇧 EN</button>
+      </div>
+    </nav>
+    <router-view />
+  </div>
+</template>
